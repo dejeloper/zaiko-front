55a78dfc2c495f3feb82fdb835dad90ac6812518
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-03-18 10:20:36 -0500

    Updated AppLayout with Sidebar and Header

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import Head from 'next/head';
 
-// import { Sidebar } from './_AppLayout/sidebar';
-// import { Header } from './_AppLayout/header';
+import { Sidebar } from './App/sidebar';
+import { Header } from './App/header';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -19,11 +19,9 @@ export const AppLayout = ({ children }: AppLayoutProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className="flex h-screen overflow-hidden">
-        SideBar
-        {/* <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-          Header
-          {/* <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <main>
             <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
               {children}
