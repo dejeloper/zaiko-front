a37640b33bc510a171cfe0bdf3313db298af4ebc
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-03-18 09:01:37 -0500

    Updated package.json and PostCSS configurations

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import tailwindConfig from './src/css/tailwind.config.js'
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  plugins: [tailwind(tailwindConfig), autoprefixer],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
