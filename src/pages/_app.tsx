d28152688389c7cd240aa67e5aea6d30d2bd9edd
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-03-18 08:43:38 -0500

    Updated home pages with base configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    if (localStorage.getItem('sidebar-expanded') == 'true') {
+      document.body.classList.add("sidebar-expanded");
+    } else {
+      document.body.classList.remove("sidebar-expanded");
+    }
+  });
+
+  const router = useRouter();
+  const { pathname } = router;
+
+  useEffect(() => {
+    const htmlElement = document.querySelector('html');
+    if (htmlElement) {
+      htmlElement.style.scrollBehavior = 'auto';
+      window.scroll({ top: 0 })
+      htmlElement.style.scrollBehavior = '';
+    }
+  }, [pathname]);
+
   return <Component {...pageProps} />
 }
+
+export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+const Document = () => {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="es-co" translate="no">
+      <Head>
+        <link rel="icon" href="/icon.png" sizes="32x32" />
+      </Head>
+      <body className="font-inter antialiased bg-slate-100 text-slate-600">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Document;
