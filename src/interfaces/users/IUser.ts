69920c9f6a8abee9e743aa93aed290642324eb80
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-03-26 20:36:30 -0500

    Update user API: Add logic to save user_contacts

--- src/interfaces/users/IUser.ts
+++ src/interfaces/users/IUser.ts
@@ -8,17 +8,22 @@ export interface IUser {
   type_document_id: number;
   state_id: number;
   rol_id: number;
+  contacts: IUserContacts;
   change_password?: boolean;
   enabled?: boolean;
   user_create?: string;
   date_create?: Date;
   user_update?: string;
   date_update?: Date;
-  complements_user?: IUserComplement;
 }
 
-export interface IUserComplement {
-  address?: string[];
-  email?: string[];
-  phone?: string[];
+export interface IUserContacts {
+  id?: number;
+  address: string[];
+  email: string[];
+  phone: string[];
+  user_create?: string;
+  date_create?: Date;
+  user_update?: string;
+  date_update?: Date;
 }
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -79,44 +79,67 @@ const getAllUsers = async (): Promise<IApiResponse> => {
 }
 
 const insertOne = async (body: IUser): Promise<IApiResponse> => {
-  const { username, password, name, lastname, number_document, type_document_id, state_id, rol_id } = body;
+  const { username, password, name, lastname, number_document, type_document_id, state_id, rol_id, contacts } = body;
+  const { address, email, phone } = contacts;
 
-  const query = `INSERT INTO public.users ("username", "password", "name", "lastname", "number_document", "type_document_id", "state_id", "rol_id") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
-  const values = [username, password, name, lastname, number_document, type_document_id, state_id, rol_id];
+  const queryContacts = `INSERT INTO public.user_contacts ("address", "email", "phone") values ($1, $2, $3) RETURNING id`;
+  const valuesContacts = [JSON.stringify(address), JSON.stringify(email), JSON.stringify(phone)];
 
   try {
-    const { rows } = await conn.query(query, values);
+    const { rows } = await conn.query(queryContacts, valuesContacts);
 
-    if (rows.length) {
-      const response: IApiResponse = {
+    if (rows.length !== 1) {
+      return {
+        states: false,
+        code: 403,
+        message: 'Error: No se pudo guardar los datos de contacto del usuario, intentelo nuevamente',
+        count: 0,
+        data: {},
+      };
+    }
+
+    const user_contacts = rows[0].id;
+
+    const query = `INSERT INTO public.users ("username", "password", "name", "lastname", "number_document", "type_document_id", "state_id", "rol_id", "contacts_id") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`;
+    const values = [username, password, name, lastname, number_document, type_document_id, state_id, rol_id, user_contacts];
+
+    try {
+      const { rows } = await conn.query(query, values);
+
+      if (rows.length <= 0) {
+        return {
+          states: false,
+          code: 403,
+          message: `Error: No se guardó el usuario, intentelo nuevamente`,
+          count: 0,
+          data: {},
+        }
+      }
+
+      return {
         states: true,
         code: 201,
         message: 'Creación Exitosa',
         count: rows.length,
         data: rows,
       };
-
-      return response;
-    } else {
-      const response: IApiResponse = {
+    } catch (error: any) {
+      return {
         states: false,
         code: 403,
-        message: `Error: No se guardó el usuario, intentelo nuevamente`,
+        message: `Error: ${error.detail}`,
         count: 0,
         data: {},
       }
-
-      return response;
     }
+
   } catch (error: any) {
-    const response: IApiResponse = {
+    return {
       states: false,
       code: 403,
       message: `Error: ${error.detail}`,
       count: 0,
       data: {},
     }
-
-    return response;
   }
 }
