f8f0d40fbf0c0e891b305df6bcb57fda7539817e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-03-31 12:32:43 -0500

    Update global SWR configuration for API consumption

--- src/hooks/users/useUsers.ts
+++ src/hooks/users/useUsers.ts
@@ -1,10 +1,11 @@
 import useSWR, { SWRConfiguration } from 'swr';
 import { IApiResponse } from '@/interfaces/api';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useUsers = (url: string, config: SWRConfiguration = {}) => {
-  const { data, error, isLoading } = useSWR<IApiResponse>(`/api${url}`, fetcher, config);
+  // const { data, error, isLoading } = useSWR<IApiResponse>(`/api${url}`, fetcher, config);
+  const { data, error, isLoading } = useSWR<IApiResponse>(`/api${url}`, config);
 
   return {
     data,
