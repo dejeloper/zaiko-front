99d0e8c2595aa3f45efd05b408b4015b182fa1eb
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-03-31 00:16:14 -0500

    Create custom hook to query user API for fetching all users using SWR

---
+++ src/hooks/users/useUsers.ts
@@ -0,0 +1,18 @@
+import useSWR, { SWRConfiguration } from 'swr';
+import { IApiResponse } from '@/interfaces/api';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export const useUsers = (url: string, config: SWRConfiguration = {}) => {
+  const { data, error, isLoading } = useSWR<IApiResponse>(`/api${url}`, fetcher, config);
+
+  return {
+    data,
+    isLoading,
+    error
+  }
+
+}
+
+
+
