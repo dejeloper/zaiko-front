d4db1592c6b497962a9656df3af963e55454b9c3
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-06 21:04:02 -0500

    Update Users page and Create New User page with Layout change

--- src/pages/settings/Users.tsx
+++ src/pages/settings/Users.tsx
@@ -8,38 +8,33 @@ import Link from "next/link";
 const Users = () => {
   const { data, isLoading, error } = useUsers('/users');
   return (
-    <AppLayout title="Zaiko - Usuarios">
-      <div className="col-span-full xl:col-span-6 bg-slate-100 text-slate-700 dark:bg-slate-900 dark:text-slate-300 shadow-xl rounded-sm border border-none">
-        <header className="px-5 py-4 border-b border-slate-200 dark:border-slate-800">
-          <h1 className="font-semibold text-2xl">Usuarios</h1>
-        </header>
-        <div className="p-3">
-          <div className="flex justify-end items-center">
-            <Link href={`/settings/Users/Add`} passHref className="inline-flex items-center bg-primary text-primaryText hover:bg-primaryHard font-bold py-2 px-4 rounded">
-              <RiUserAddLine className="mr-2" />
-              <span>Agregar Usuario</span>
-            </Link>
-          </div>
-          {
-            isLoading
-              ? (
-                <FullScreenLoading />
-              )
-              : !data
-                ? (
-                  <p className="w-full first-letter:p-2 text-center text-white text-bold">
-                    No hay información
-                  </p>
-                )
-                : (
-                  <>
-                    <Table users={data} />
-                    <Cards users={data} />
-                  </>
-                )
-          }
+    <AppLayout title="Zaiko - Usuarios" titlePage="Lista de Usuarios">
+      <div className="p-3">
+        <div className="flex justify-end items-center">
+          <Link href={`/settings/Users/Add`} passHref className="inline-flex items-center bg-primary text-primaryText hover:bg-primaryHard font-bold py-2 px-4 rounded">
+            <RiUserAddLine className="mr-2" />
+            <span>Agregar Usuario</span>
+          </Link>
         </div>
-      </div >
+        {
+          isLoading
+            ? (
+              <FullScreenLoading />
+            )
+            : !data
+              ? (
+                <p className="w-full first-letter:p-2 text-center text-white text-bold">
+                  No hay información
+                </p>
+              )
+              : (
+                <>
+                  <Table users={data} />
+                  <Cards users={data} />
+                </>
+              )
+        }
+      </div>
     </AppLayout >
   )
 }
--- src/pages/settings/Users/Add.tsx
+++ src/pages/settings/Users/Add.tsx
@@ -60,113 +60,103 @@ const Add = () => {
   };
 
   return (
-    <AppLayout title="Zaiko - Agregar Usuarios">
-      <div className="col-span-full xl:col-span-6 bg-slate-100 text-slate-700 dark:bg-slate-900 dark:text-slate-300 shadow-xl rounded-sm border border-none">
-        <header className="px-5 py-4 border-b border-slate-200 dark:border-slate-800">
-          <h1 className="font-semibold text-2xl">Crear Usuario</h1>
-        </header>
-        <Toaster position="top-center" />
-        <div className="p-3">
-          <div className="container mx-auto text-sm w-4/5">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="w-full p-4 px-5 py-2">
-                <div className="sm:grid md:grid-cols-2 lg:grid-cols-2 gap-4">
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Usuario
-                        <input className="inputFormBase" type="text" {...register("username", { required: 'El Usuario es obligatorio', minLength: { value: 4, message: 'El Usuario debe tener mínimo 4 caracteres' }, maxLength: { value: 20, message: 'El Usuario debe tener máximo 20 caracteres' } })} placeholder="Usuario" />
-                        <p>{errors.username?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <label className="hidden">Separate</label>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Nombre
-                        <input className="inputFormBase" type="text" {...register("name", { required: 'El Nombre es obligatorio', minLength: { value: 4, message: 'El Nombre debe tener mínimo 4 caracteres' }, maxLength: { value: 20, message: 'El Nombre debe tener máximo 20 caracteres' } })} placeholder="Nombre" />
-                        <p>{errors.name?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Apellido
-                        <input className="inputFormBase" type="text" {...register("lastname", { required: 'El Apellido es obligatorio', minLength: { value: 4, message: 'El Apellido debe tener mínimo 4 caracteres' }, maxLength: { value: 20, message: 'El Apellido debe tener máximo 20 caracteres' } })} placeholder="Apellido" />
-                        <p>{errors.lastname?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Tipo de Documento
-                        <select className="inputFormBase" {...register("type_document_id", { required: 'El Tipo de Documento es obligatorio' })}>
-                          <option hidden value="">Tipo de Documento</option>
-                          <option value="1">Cédula de Ciudadanía</option>
-                          <option value="2">Cédula de Extranjería</option>
-                          <option value="3">Pasaporte</option>
-                          <option value="4">Tajeta de Identidad</option>
-                          <option value="5">Registro Civil</option>
-                        </select>
-                        <p>{errors.type_document_id?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Número de Documento
-                        <input className="inputFormBase" type="text" {...register("number_document", { required: 'El Número de Documento es obligatorio', minLength: { value: 6, message: 'El Nombre debe tener mínimo 6 caracteres' }, maxLength: { value: 15, message: 'El Nombre debe tener máximo 15 caracteres' } })} placeholder="Número de Documento" />
-                        <p>{errors.number_document?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Estado
-                        <select className="inputFormBase" {...register("state_id", { required: 'El Estado es obligatorio' })}>
-                          <option hidden value="">Estado</option>
-                          <option value="2">Activo</option>
-                          <option value="3">Inactivo</option>
-                          <option value="4">Bloqueado</option>
-                        </select>
-                        <p>{errors.state_id?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-1">
-                    <div className="mb-1">
-                      <label className="labelFormBase">
-                        Perfil
-                        <select className="inputFormBase" {...register("rol_id", { required: 'El Perfil es obligatorio' })}>
-                          <option hidden value="">Perfil</option>
-                          <option value="2">Administrador</option>
-                          <option value="3">Gestor</option>
-                          <option value="4">Empleado</option>
-                          <option value="5">Temporal</option>
-                        </select>
-                        <p>{errors.rol_id?.message}</p>
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-span-2 sm:col-span-2 md:col-span-2 flex justify-end items-center gap-4">
-                    <button type="submit" className="btn-xs text-white bg-successHard hover:bg-success px-5 py-2">
-                      Guardar
-                    </button>
-                    <button type="reset" className="btn-xs text-white bg-secondaryHard hover:bg-secondary px-5 py-2">
-                      Limpiar
-                    </button>
+    <AppLayout title="Zaiko - Agregar Usuarios" titlePage="Crear Usuario">
+      <div className="p-3">
+        <div className="container mx-auto text-sm w-4/5">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="w-full p-4 px-5 py-2">
+              <div className="sm:grid md:grid-cols-2 lg:grid-cols-2 gap-4">
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Usuario
+                      <input className="inputFormBase" type="text" {...register("username", { required: 'El Usuario es obligatorio', minLength: { value: 4, message: 'El Usuario debe tener mínimo 4 caracteres' }, maxLength: { value: 20, message: 'El Usuario debe tener máximo 20 caracteres' } })} placeholder="Usuario" />
+                      <p>{errors.username?.message}</p>
+                    </label>
                   </div>
                 </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <label className="hidden">Separate</label>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Nombre
+                      <input className="inputFormBase" type="text" {...register("name", { required: 'El Nombre es obligatorio', minLength: { value: 4, message: 'El Nombre debe tener mínimo 4 caracteres' }, maxLength: { value: 20, message: 'El Nombre debe tener máximo 20 caracteres' } })} placeholder="Nombre" />
+                      <p>{errors.name?.message}</p>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Apellido
+                      <input className="inputFormBase" type="text" {...register("lastname", { required: 'El Apellido es obligatorio', minLength: { value: 4, message: 'El Apellido debe tener mínimo 4 caracteres' }, maxLength: { value: 20, message: 'El Apellido debe tener máximo 20 caracteres' } })} placeholder="Apellido" />
+                      <p>{errors.lastname?.message}</p>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Tipo de Documento
+                      <select className="inputFormBase" {...register("type_document_id", { required: 'El Tipo de Documento es obligatorio' })}>
+                        <option hidden value="">Tipo de Documento</option>
+                        <option value="1">Cédula de Ciudadanía</option>
+                        <option value="2">Cédula de Extranjería</option>
+                        <option value="3">Pasaporte</option>
+                        <option value="4">Tajeta de Identidad</option>
+                        <option value="5">Registro Civil</option>
+                      </select>
+                      <p>{errors.type_document_id?.message}</p>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Número de Documento
+                      <input className="inputFormBase" type="text" {...register("number_document", { required: 'El Número de Documento es obligatorio', minLength: { value: 6, message: 'El Nombre debe tener mínimo 6 caracteres' }, maxLength: { value: 15, message: 'El Nombre debe tener máximo 15 caracteres' } })} placeholder="Número de Documento" />
+                      <p>{errors.number_document?.message}</p>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Estado
+                      <select className="inputFormBase" {...register("state_id", { required: 'El Estado es obligatorio' })}>
+                        <option hidden value="">Estado</option>
+                        <option value="2">Activo</option>
+                        <option value="3">Inactivo</option>
+                        <option value="4">Bloqueado</option>
+                      </select>
+                      <p>{errors.state_id?.message}</p>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-1">
+                  <div className="mb-1">
+                    <label className="labelFormBase">
+                      Perfil
+                      <select className="inputFormBase" {...register("rol_id", { required: 'El Perfil es obligatorio' })}>
+                        <option hidden value="">Perfil</option>
+                        <option value="2">Administrador</option>
+                        <option value="3">Gestor</option>
+                        <option value="4">Empleado</option>
+                        <option value="5">Temporal</option>
+                      </select>
+                      <p>{errors.rol_id?.message}</p>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-2 sm:col-span-2 md:col-span-2 flex justify-end items-center gap-4">
+                  <input type="submit" className="inline-flex items-center bg-successHard text-primaryText hover:bg-success font-bold py-2 px-4 rounded" value="Guardar" />
+                  <input type="reset" className="inline-flex items-center bg-secondaryHard hover:bg-secondary text-primaryText   font-bold py-2 px-4 rounded" value="Limpiar" />
+                </div>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
     </AppLayout>
